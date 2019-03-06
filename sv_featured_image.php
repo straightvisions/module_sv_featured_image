96f9bfa2224d5454d1ab5375f2459d42eb847ead
Matthias Reuter <matthias-reuter@straightvisions.com>
2019-03-06 19:49:43 +0100

    MIGRATE v2

--- sv_featured_image.php
+++ sv_featured_image.php
@@ -73,11 +73,11 @@ class sv_featured_image extends init {
 
 		// Load Styles
 		static::$scripts->create( $this )
-			->set_source( $this->get_file_url( 'lib/css/frontend.css' ), $this->get_file_path( 'lib/css/frontend.css' ) )
+			->set_path( 'lib/css/frontend.css' )
 			->set_inline($settings['inline']);
 
 		ob_start();
-		include( $this->get_file_path( 'lib/tpl/frontend.php' ) );
+		include( $this->get_path( 'lib/tpl/frontend.php' ) );
 		$output									= ob_get_contents();
 		ob_end_clean();
 
